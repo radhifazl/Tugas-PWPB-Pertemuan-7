2f83aa8d23fd94a0482652ff5632fe9a28a5c38f
Radhif Azli <77906566+radhif-zyn@users.noreply.github.com>
2021-10-06 09:39:42 +0700

    Add files via upload

---
+++ pertemuan7/Tugas 7/pertemuan7.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Pertemuan 7</title>
+</head>
+<body>
+    <div class="menu">
+        <a href="#">Home</a>
+        <a href="#">Profile</a>
+        <a href="#">Contact</a>
+    </div>
+
+    <div class="banner">
+        <div class="isi-banner">
+            <h1 class="banner-title">Hai, namaku</h1>
+            <h1 class="banner-name">Radhif Azli Nurfahriza</h1>
+            <button class="banner-btn"> Read More </button>
+        </div>
+    </div>
+
+    <div class="body">
+        <div id="body-left">
+            <div class="bl-content">
+                <h1 id="kelas">XI RPL 1</h1>    
+            </div>
+        </div>
+
+        <div id="body-right">
+            <div class="br-content">
+                <h1>Tugas <br> Pertemuan <br> 7</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div id="footer-left">
+            <a href="">Radhif Azli Nurfahriza</a>
+        </div>
+
+        <div id="footer-right">
+            <a href="#">Home</a>
+            <a href="#">Profile</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ pertemuan7/latihan1.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>External CSS - 7</title>
+        <link rel="stylesheet" type="text/css" href="contoh.css">
+    </head>
+    <body>
+
+        <div class="menu">
+            <a href="">Home</a>&nbsp;|
+            <a href="">Profile</a>&nbsp;|
+            <a href="">Contact</a>
+        </div>
+
+
+        <div class="banner">
+        </div>
+
+        <div class="body">
+            <div id="b_left">
+            </div>
+            <div id="b_right">
+            </div>
+        </div>
+
+
+        <div class="footer">
+            <div id="f_left">
+            </div>
+            <div id="f_right">
+                <a href="">Home</a>&nbsp;|
+                <a href="">Profile</a>&nbsp;|
+                <a href="">Contact</a>
+            </div>
+        </div>
+
+    </body>
+</html>
